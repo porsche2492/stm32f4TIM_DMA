96e48c309c7dbcb591004359513c3fdd8cb2a75c
zeertix <anton.ivanenkov@gmail.com>
2019-09-02 19:19:13 +0300

    Revert "1"
    This reverts commit b31818a743c151343b66a5c2085a9d0111dbe23f.

--- SOURCE/RS232.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RS232_H
-#define __RS232_H
-#include "stdint.h"
-
-void RS232Init(void);
-void RS232Send(uint8_t data);
-void RS232SendString(char *str);
-
-#endif /* __RS232_H */
--- SOURCE/timers.h
+++
@@ -1,7 +0,0 @@
-#ifndef __TIMERS_H
-#define __TIMERS_H
-#include "stdint.h"
-
-void TimInit(void);
-
-#endif /* __TIMERS_H */
